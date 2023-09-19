217020c2fc967d9b4b85a6c335b0ea0870ed626d
Jhcki222 <iqbox2@naver.com>
2023-09-19 13:30:08 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <Link to ="/">Home</Link>
+        <Link to ="/about">Home</Link>
+      </nav>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
